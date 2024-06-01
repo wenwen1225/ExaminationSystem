2b396c38f4811065eb627875494e068f0b803703
wen <11033130@gm.chihlee.edu.tw>
2024-06-01 15:02:57 +0800

    update score

--- login.php
+++ login.php
@@ -9,13 +9,13 @@
 			'example');  //連線資料庫名稱
 	
 	$sql = "SELECT * FROM students WHERE id='$id' AND pwd='$pwd'";		
-	
 	if( $result = mysqli_query($link, $sql)){
 		$total_records=mysqli_num_rows($result);
 		if($total_records>0){
 			$row = mysqli_fetch_assoc($result);
 			$name = $row["name"];
-			echo '<h1>' . $name . '您好</h1>';
+			echo '<h1>' . $name . ' 您好</h1>';
+			echo '<a href="student1.php">進入考試頁面</a>';
 		}else{
 			echo '帳/密錯誤<br><a href=login.html>回登入畫面</a>';
 		}
